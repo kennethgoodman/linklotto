508eb39bdb215502fb228d8bebeaf2823edd4e0e
kennethgoodman <kennethsgoodman@yahoo.com>
2021-08-05 13:27:38 -0400

    adding function call that was missing

--- backend/dynamodb.py
+++ backend/dynamodb.py
@@ -10,7 +10,7 @@ def get_aws_credentials():
 def get_dynamodb():
 	global dynamodb
 	if not dynamodb:
-		aws_access_key_id, aws_secret_access_key = get_aws_credentials
+		aws_access_key_id, aws_secret_access_key = get_aws_credentials()
 		dynamodb = boto3.resource('dynamodb', 
 								region_name='us-east-2',
 								aws_access_key_id=aws_access_key_id,
