32ce76cfbb01ed28d42606758ff2e07553dbbeda
kennethgoodman <kennethsgoodman@yahoo.com>
2021-08-05 13:31:07 -0400

    stripping new line

--- backend/dynamodb.py
+++ backend/dynamodb.py
@@ -3,8 +3,8 @@ dynamodb = None
 
 def get_aws_credentials():
 	with open('aws.credentials.csv', 'r') as f:
-		aws_access_key_id = f.readline().split("=")[1]
-		aws_secret_access_key = f.readline().split("=")[1]
+		aws_access_key_id = f.readline().split("=")[1].strip()
+		aws_secret_access_key = f.readline().split("=")[1].strip()
 	return (aws_access_key_id, aws_secret_access_key)
 
 def get_dynamodb():
