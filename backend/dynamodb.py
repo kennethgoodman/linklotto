efc5504eae331264d75a50e5962ced106846a138
kennethgoodman <kennethsgoodman@yahoo.com>
2021-08-05 13:06:55 -0400

    adding dynamodb client

---
+++ backend/dynamodb.py
@@ -0,0 +1,18 @@
+
+dynamodb = None
+
+def get_aws_credentials():
+	with open('aws.credentials.csv', 'r') as f:
+		aws_access_key_id = f.readline().split("=")[1]
+		aws_secret_access_key = f.readline().split("=")[1]
+	return (aws_access_key_id, aws_secret_access_key)
+
+def get_dynamodb():
+	if not dynamodb:
+		aws_access_key_id, aws_secret_access_key = get_aws_credentials
+		dynamodb = boto3.resource('dynamodb', 
+								region_name='us-east-2',
+								aws_access_key_id=aws_access_key_id,
+								aws_secret_access_key=aws_secret_access_key,
+		)
+	return dynamodb
